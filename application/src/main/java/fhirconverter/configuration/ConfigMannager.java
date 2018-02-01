0d2b4aa2dd2e55783c4df5200015a85d7a395144
Chenghui Fan <ucabcf0@ucl.ac.uk>
2018-02-01 22:12:38 +0000

    deleted some unnecessary methods

--- application/src/main/java/fhirconverter/configuration/ConfigMannager.java
+++ application/src/main/java/fhirconverter/configuration/ConfigMannager.java
@@ -1,5 +1,6 @@
 package fhirconverter.configuration;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigMannager {
@@ -7,35 +8,39 @@ public class ConfigMannager {
 	static String DEVELOPING = "Developing";
 	static String TESTING = "Testing";
 	static String PRODUCTION = "Production";
+	static String MAPPING = "Mapping";
+	static String DATABASE = "Database";
 	
-	private int environment;
+	private String environment;
 	private ConfigFilePathMannager configFilePathMannager;
 	
 	//structure: <environment, <configType, configObject>>
-	private Map<Integer, Map<String, Config>>registeredConfig;
+	private Map<String, Map<String, Config>>registeredConfig;
 	
-	public ConfigMannager(int environment){
-		this.environment = environment;
+	public ConfigMannager(String environment){
 		this.configFilePathMannager = new ConfigFilePathMannager();
-	}
-	
-	private void loadConfig() {
 		
 	}
 	
-	public Object getMapping() {
-		return null;
-	}
-	
-	public void addConfig(String configType, String filePath, boolean isReadable, boolean isWritable){
-		Config config;
-		switch(configType){
-			case "mapping":
-				
+	private void loadConfig(String environment) {
+		MappingConfig mappingConfig = new MappingConfig(this.configFilePathMannager.getFilePath(this.environment, MAPPING));
+		if(mappingConfig == null) {
+			System.out.println("mappingConfig loading failed");
+		}
+		else {
+			Map<String, Config> mappingConfigs = this.registeredConfig.get(MAPPING);
+			mappingConfigs = mappingConfigs == null ? new HashMap<>() : mappingConfigs;
+			mappingConfigs.put(MAPPING, mappingConfig);
+			this.registeredConfig.put(this.environment, mappingConfigs);
 		}
 	}
 	
-	private void addConfigToMap(){
-		
+	public Object getMappingResult(String key) {
+		Config mappingConfig = this.registeredConfig.get(this.environment).get(MAPPING);
+		if(mappingConfig instanceof MappingConfig)
+			return ((MappingConfig) mappingConfig).getMappingResult(key);
+		else
+			return null;
 	}
+	
 }
