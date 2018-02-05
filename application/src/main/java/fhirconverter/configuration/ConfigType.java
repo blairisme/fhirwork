2212f22566bcd5b0f56ef3e9fc4f5da6a0e1117e
psycf1 <psycf1@exmail.nottingham.ac.uk>
2018-02-05 22:55:11 +0000

    Added enum classes to replace some static values

---
+++ application/src/main/java/fhirconverter/configuration/ConfigType.java
@@ -0,0 +1,5 @@
+package fhirconverter.configuration;
+
+public enum ConfigType {
+	NETWORK, MAPPING, DATABASE;   
+}
---
+++ application/src/main/java/fhirconverter/configuration/Environment.java
@@ -0,0 +1,5 @@
+package fhirconverter.configuration;
+
+public enum Environment {
+	TESTING, DEVELOPMENT, PRODUCTION
+}
